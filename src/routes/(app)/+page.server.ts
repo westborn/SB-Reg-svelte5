bb2693fc260217e329e2704fe46f66842e780c7d
George Stone <george@westborn.com.au>
2024-04-28 10:35:47 +1000

    Working withn all logs removed

---
+++ src/routes/(app)/+page.server.ts
@@ -0,0 +1,10 @@
+import { superValidate } from 'sveltekit-superforms';
+import type { Actions, PageServerLoad } from './$types';
+import { zod } from 'sveltekit-superforms/adapters';
+import { redirect } from '@sveltejs/kit';
+
+export const load: PageServerLoad = async (event) => {
+	const { session, user } = await event.locals.safeGetSession();
+	if (!user) redirect(302, '/login'); //no user - signup or login
+	return { session, user };
+};
