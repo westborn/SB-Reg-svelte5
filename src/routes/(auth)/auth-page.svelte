2308be13af1e35cb0992c9f226b42ba59c7d5992
George Stone <george@westborn.com.au>
2024-10-24 11:21:35 +1100

    fix: auth nav bar

--- src/routes/(auth)/auth-page.svelte
+++ src/routes/(auth)/auth-page.svelte
@@ -13,8 +13,13 @@
 	let { type, children, session, user }: Props = $props();
 </script>
 
-<div class="mx-auto max-w-xl sm:container">
-	<div class="absolute right-2 top-2">
+<div class="container mx-auto grid h-14 max-w-2xl grid-cols-[1fr_250px] items-center">
+	<!-- <div class="mx-auto max-w-xl sm:container"> -->
+	<div class="flex items-center">
+		<img src="/favicon-32x32.png" alt="Sculpture Bermagui Logo" class="mt-2 h-10" />
+		<h1 class=" px-2 text-center text-lg text-primary-400">Exhibition Registration</h1>
+	</div>
+	<div class="">
 		<div class="flex items-center gap-2">
 			{#if type === 'login'}
 				<Button href="/signup" variant="ghost">Register</Button>
@@ -28,24 +33,24 @@
 				<Button href="/login" variant="ghost">Login</Button>
 				<Button href="/signup" variant="ghost">Register</Button>
 			{/if}
-			<ThemeToggle />
+			<!-- <ThemeToggle /> -->
 		</div>
 	</div>
-	<div class="mx-auto mt-20 w-full max-w-sm">
-		{@render children()}
-		<p class="p-4 text-sm text-muted-foreground">
-			By clicking continue, you agree to our{' '}
-			<a
-				href="https://sculpturebermagui.org.au/wp-content/uploads/2024/10/Exhibiting-at-Sculpture-Bermagui-2025.pdf"
-				class="underline underline-offset-4 hover:text-primary"
-			>
-				Terms of Service</a
-			>
-			{' '}and{' '}
-			<a
-				href="https://sculpturebermagui.org.au/wp-content/uploads/2024/02/Edgy-Art-Privacy-Policy-Sep-2022.pdf"
-				class="underline underline-offset-4 hover:text-primary">Privacy Policy.</a
-			>
-		</p>
-	</div>
+</div>
+<div class="mx-auto mt-20 w-full max-w-sm">
+	{@render children()}
+	<p class="p-4 text-sm text-muted-foreground">
+		By clicking continue, you agree to our{' '}
+		<a
+			href="https://sculpturebermagui.org.au/wp-content/uploads/2024/10/Exhibiting-at-Sculpture-Bermagui-2025.pdf"
+			class="underline underline-offset-4 hover:text-primary"
+		>
+			Terms of Service</a
+		>
+		{' '}and{' '}
+		<a
+			href="https://sculpturebermagui.org.au/wp-content/uploads/2024/02/Edgy-Art-Privacy-Policy-Sep-2022.pdf"
+			class="underline underline-offset-4 hover:text-primary">Privacy Policy.</a
+		>
+	</p>
 </div>
