9562d8d23daa1ada29c71fe6b90e80ae4a7b1557
George Stone <george@westborn.com.au>
2025-12-05 15:14:08 +1100

    fix: update Square SDK import and use legacy module for compatibility

--- src/routes/api/payment/+server.ts
+++ src/routes/api/payment/+server.ts
@@ -1,5 +1,7 @@
 import { json } from '@sveltejs/kit';
-import { Client, Environment } from 'square';
+// https://github.com/square/square-nodejs-sdk/blob/e66c2d9e32225b800be6d7f15ef9a5d9f5d516aa/README.md
+// TODO: switch to modern SDK when time is available
+import { Client, Environment } from 'square/legacy';
 import { randomUUID } from 'crypto';
 import { SECRET_SQUARE_ACCESS_TOKEN } from '$env/static/private';
 import { PUBLIC_SQUARE_ENVIRONMENT } from '$env/static/public';
